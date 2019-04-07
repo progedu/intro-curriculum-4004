a24fadb36a40d9fa2cb0267c579467483dda7eaf
kensyo <takitateokowa@gmail.com>
2019-04-07 13:41:48 +0900

    ダメージ値を四捨五入

--- index.js
+++ index.js
@@ -15,10 +15,10 @@
  * @return {Number} ダメージ
  */
 function effectiveDamage(power, armor, armorPenetration) {
-  let effectiveArmor = normalize(armor) - normalize(armorPenetration);
-  effectiveArmor = effectiveArmor <= 0 ? 0 : effectiveArmor;
-  const damageDecrease = effectiveArmor / (100 + effectiveArmor);
-  return Math.floor(normalize(power) * (1 - damageDecrease));
+    let effectiveArmor = normalize(armor) - normalize(armorPenetration);
+    effectiveArmor = effectiveArmor <= 0 ? 0 : effectiveArmor;
+    const damageDecrease = effectiveArmor / (100 + effectiveArmor);
+    return Math.round(normalize(power) * (1 - damageDecrease));
 }
 
 /**
@@ -27,15 +27,15 @@ function effectiveDamage(power, armor, armorPenetration) {
  * @return {Number} 正常値
  */
 function normalize(n) {
-  if (n < 0) {
-    return 0;
-  } else if (n >= 2000) {
-    return 2000;
-  } else {
-    return n;
-  }
+    if (n < 0) {
+        return 0;
+    } else if (n >= 2000) {
+        return 2000;
+    } else {
+        return n;
+    }
 }
 
 module.exports = {
-  effectiveDamage: effectiveDamage
+    effectiveDamage: effectiveDamage
 };
