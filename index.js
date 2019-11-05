3e7a5a2c51f885f6d0ac262da7d9f75655d31189
gtpmm <bluephysics33@gmail.com>
2019-11-06 08:33:43 +0900

    ダメージ小数点以下四捨五入整数化実装

--- index.js
+++ index.js
@@ -18,7 +18,9 @@ function effectiveDamage(power, armor, armorPenetration) {
   let effectiveArmor = normalize(armor) - normalize(armorPenetration);
   effectiveArmor = effectiveArmor <= 0 ? 0 : effectiveArmor;
   const damageDecrease = effectiveArmor / (100 + effectiveArmor);
-  return Math.floor(normalize(power) * (1 - damageDecrease));
+  //return Math.floor(normalize(power) * (1 - damageDecrease));
+  return Math.round(normalize(power) * (1 - damageDecrease));
+
 }
 
 /**
@@ -27,6 +29,7 @@ function effectiveDamage(power, armor, armorPenetration) {
  * @return {Number} 正常値
  */
 function normalize(n) {
+  //n= Math.round(n);
   if (n < 0) {
     return 0;
   } else if (n >= 2000) {
