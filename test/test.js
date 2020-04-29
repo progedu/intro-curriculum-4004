b7522dad7b525a28cb16d74002b6d6664a7243ce
kazumaisamatsu <54142720+kazumaisamatsu@users.noreply.github.com>
2020-04-30 04:59:43 +0900

    課題

--- test/test.js
+++ test/test.js
@@ -13,19 +13,17 @@ describe('#effectiveDamage()', () => {
     assert.equal(dc.effectiveDamage(0, -1, 0), 0);
     assert.equal(dc.effectiveDamage(0, 0, -1), 0);
   });
-
   it('2000より大きい異常値におけるダメージ計算ができる', () => {
-    assert.equal(dc.effectiveDamage(2001, 0, 0), 2000);
-    assert.equal(dc.effectiveDamage(300, 2001, 0), 14);
-    assert.equal(dc.effectiveDamage(300, 2000, 2001), 300);
-  });
-
+   assert.equal(dc.effectiveDamage(2001, 0, 0), 2000);
+   assert.equal(dc.effectiveDamage(300, 2150, 0), 14);
+   assert.equal(dc.effectiveDamage(300, 2000, 2001), 300);
+ });  
+  
   it('実効防御力は0未満にならない', () => {
-    assert.equal(dc.effectiveDamage(500, 100, 800), 500);
-  });
-
+　  assert.equal(dc.effectiveDamage(500, 100, 800), 500);
+　  });
   it('ダメージは小数点以下を四捨五入して整数にする', () => {
-    assert.equal(dc.effectiveDamage(620, 100, 30), 365);
-  });
+      assert.equal(dc.effectiveDamage(620, 100, 30), 365);
+    });
 
 });
