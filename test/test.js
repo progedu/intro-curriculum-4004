1e204a1fe8ef441a0e95cb3af105327f155361c7
Shuntaro <shun_harutaro@hm.aitai.ne.jp>
2020-05-13 23:12:46 +0900

    正しく四捨五入されるように訂正

--- test/test.js
+++ test/test.js
@@ -27,5 +27,9 @@ describe('#effectiveDamage()', () => {
   it('ダメージは小数点以下を四捨五入して整数にする', () => {
     assert.equal(dc.effectiveDamage(620, 100, 30), 365);
   });
+  
+  it('ダメージは小数点以下を四捨五入して整数にする', () => {
+    assert.equal(dc.effectiveDamage(620, 100, 30), 365);
+  });
 
 });
